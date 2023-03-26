987c02a9fd3e8742563869a0d009925c4d932c27
harousaidi8 <harounsaidi8@gmail.com>
2023-03-26 14:49:34 +0100

    add feature load jokes

--- app/src/main/java/com/po4yka/foodrecipes/util/Constants.kt
+++ app/src/main/java/com/po4yka/foodrecipes/util/Constants.kt
@@ -6,9 +6,11 @@ class Constants {
 
         const val BASE_URL = "https://api.spoonacular.com"
         const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
-       // const val API_KEY = "12ed6e19a48b4c2badc6c678d7d6757b"
+
+        // const val API_KEY = "12ed6e19a48b4c2badc6c678d7d6757b"
         const val API_KEY = "12ed6e19a48b4c2badc6c678d7d6757b"
         const val RECIPE_RESULT_KEY = "recipeBundle"
+        const val JOKES = "jokes"
 
         // API Query Keys
         const val QUERY_SEARCH = "query"
